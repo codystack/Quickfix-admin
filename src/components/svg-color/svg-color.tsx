5631a27e46a9d8f05e185cd9eba89ce3bc5c5107
codystack <ebuzzadvert@gmail.com>
2024-11-01 03:56:55 +0100

    new commmit. first commit

---
+++ src/components/svg-color/svg-color.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+
+import Box from '@mui/material/Box';
+
+import { svgColorClasses } from './classes';
+
+import type { SvgColorProps } from './types';
+
+// ----------------------------------------------------------------------
+
+export const SvgColor = React.forwardRef<HTMLSpanElement, SvgColorProps>(
+  ({ src, width = 24, height, className, sx, ...other }, ref) => (
+    <Box
+      ref={ref}
+      component="span"
+      className={svgColorClasses.root.concat(className ? ` ${className}` : '')}
+      sx={{
+        width,
+        flexShrink: 0,
+        height: height ?? width,
+        display: 'inline-flex',
+        bgcolor: 'currentColor',
+        mask: `url(${src}) no-repeat center / contain`,
+        WebkitMask: `url(${src}) no-repeat center / contain`,
+        ...sx,
+      }}
+      {...other}
+    />
+  )
+);
