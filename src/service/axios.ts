4c71fb87467e61a6563ccce58aaeb27444ec9c84
Styneman <stanleynyekpeye@gmail.com>
2025-02-13 15:28:32 +0100

    fixedd basse url issue

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-export const baseURL = "http://192.168.2.112:5050" //
-// 'https://quick-fix-api.vercel.app';
-// 
+export const baseURL = 'https://quick-fix-api.vercel.app';
+// "http://192.168.2.112:5050" //
 
 const axiosInstance = axios.create({
   baseURL: `${baseURL}/api/v1`,
